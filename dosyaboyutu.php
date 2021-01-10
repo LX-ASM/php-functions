ece9f977590189a7e4215a7a723422d432f81402
MatsuKobayashi <77240309+MatsuKobayashi@users.noreply.github.com>
2021-01-10 20:21:31 +0300

    Update dosyaboyutu.php

--- dosyaboyutu.php
+++ dosyaboyutu.php
@@ -1,21 +1,34 @@
+<?php
 function dosyaboyutu($dosya){
+
+if(substr($dosya, 0, 2)=="./")
+$dosya = substr($dosya, 2);
+else if(substr($dosya, 0, 1)!="/")
+$dosya = "/".$dosya;
+
 $dsya = filesize($dosya);
+
 if($dsya>1023){
+
 $dsya = (int)($dsya / 1024);
 $byte = $dsya % 2014;
 }
+
 if($dsya>1023){
 $dsya = (int)($dsya / 1024);
 $kb = $dsya % 1024;
 }
+
 if($dsya>1023){
 $dsya = (int)($dsya / 1024);
 $mb = $dsya % 1024;
 }
+
 if($dsya>1023){
 $dsya = (int)($dsya / 1024);
 $gb = $dsya % 1024;
 }
+
 if(isset($byte))
 return $dsya." KB + ".$byte." Bytes";
 else if(isset($kb))
@@ -26,4 +39,6 @@ else if(isset($gb))
 return $dsya." TB + ".$gb." GB + ".$mb." MB + ".$kb."KB + ".$byte." Bytes";
 else
 return $dsya." Bytes";
+
 }
+?>
